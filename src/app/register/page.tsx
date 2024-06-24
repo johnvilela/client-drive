77f36fb6e143531ee68ad8af2252b0e834bf411d
John Vilela <johnvvilela@gmail.com>
2024-06-24 16:47:34 -0300

    feat: add registerPage

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -9,10 +9,42 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import useMutate from '@/lib/hooks/useMutate';
+import { CreateUserDTO, UserType, userModule } from '@/lib/modules/userModule';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-export default function RegisterPage() {
-  const { register, handleSubmit } = useForm();
+export default function RegisterPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const email = searchParams.email as string;
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<z.infer<typeof CreateUserDTO>>({
+    resolver: zodResolver(CreateUserDTO),
+    defaultValues: {
+      email,
+    },
+  });
+  const { mutate, status, data } = useMutate({
+    method: userModule.createUser,
+  });
+
+  useEffect(() => {
+    if (status === 'success') {
+      if (data === UserType.ADMIN) return router.push('/admin/dashboard');
+      if (data === UserType.CLIENT) return router.push('/client/dashboard');
+    }
+  }, [data, router, status]);
 
   return (
     <div className="w-screen h-screen grid place-items-center p-4">
@@ -25,15 +57,18 @@ export default function RegisterPage() {
         </CardHeader>
         <CardContent>
           <form
-            onSubmit={handleSubmit((data) => console.log(data))}
+            onSubmit={handleSubmit((data) => mutate(data))}
             className="flex flex-col gap-4"
           >
             <p>
               Email:
-              <strong> ba.john.vidal@gmail.com</strong>
+              <strong>{` ${email}`}</strong>
             </p>
-            <Input type="email" label="Email" {...register('email')} />
-            <Button type="submit">Create password</Button>
+            <Input type="hidden" {...register('email')} />
+            <Input type="password" label="Password" {...register('password')} />
+            <Button type="submit" isLoading={status === 'loading'}>
+              Create password
+            </Button>
           </form>
         </CardContent>
       </Card>
--- src/lib/modules/userModule.ts
+++ src/lib/modules/userModule.ts
@@ -1,5 +1,6 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../services/firebase';
+import { doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { db, auth } from '../services/firebase';
 import z from 'zod';
 
 export enum EmailStatus {
@@ -8,25 +9,70 @@ export enum EmailStatus {
   PENDING = 'PENDING',
 }
 
+export enum UserType {
+  CLIENT = 'CLIENT',
+  ADMIN = 'ADMIN',
+}
+
 const UserSimpleSchema = z.object({
   isRegistered: z.boolean(),
+  type: z.nativeEnum(UserType),
 });
 
 export const CheckEmailStatusDTO = z.object({
   email: z.string().email(),
 });
 
+export const CreateUserDTO = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
 export const userModule = {
   checkEmailStatus: async ({ email }: z.infer<typeof CheckEmailStatusDTO>) => {
-    const docRef = doc(db, 'userSimple', email);
-    const docSnap = await getDoc(docRef);
+    try {
+      const docRef = doc(db, 'userSimple', email);
+      const docSnap = await getDoc(docRef);
 
-    if (!docSnap.exists()) {
+      if (!docSnap.exists()) {
+        return EmailStatus.NOT_REGISTERED;
+      }
+
+      const data = docSnap.data() as z.infer<typeof UserSimpleSchema>;
+
+      return data.isRegistered ? EmailStatus.REGISTERED : EmailStatus.PENDING;
+    } catch (err) {
+      console.error(err);
       return EmailStatus.NOT_REGISTERED;
     }
+  },
 
-    const data = docSnap.data() as z.infer<typeof UserSimpleSchema>;
+  createUser: async ({ email, password }: z.infer<typeof CreateUserDTO>) => {
+    try {
+      const userSession = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
 
-    return data.isRegistered ? EmailStatus.REGISTERED : EmailStatus.PENDING;
+      const simpleRef = doc(db, 'userSimple', email);
+
+      await updateDoc(simpleRef, { isRegistered: true });
+
+      const userSimple = await getDoc(simpleRef);
+
+      const completeRef = doc(db, 'user', userSession.user.uid);
+
+      await setDoc(completeRef, {
+        email,
+        type: userSimple.data()?.type || UserType.CLIENT,
+      });
+
+      return userSimple.data()?.type || UserType.CLIENT;
+    } catch (error) {
+      console.error(error);
+
+      return error;
+    }
   },
 };
