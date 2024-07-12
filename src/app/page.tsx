4a68b3078c85c172d04da818f54988e8aa4ab66e
John Vilela <johnvvilela@gmail.com>
2024-07-11 23:33:59 -0300

    chore: add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <h1>Home page</h1>
+      <Button>Test</Button>
+    </>
+  );
 }
