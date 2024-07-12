282f4aefed5f3459b1d5c388dc63e4d37ae0cd5c
John Vilela <johnvvilela@gmail.com>
2024-07-12 08:23:05 -0300

    refactor: improve managment structure & create client view

---
+++ src/app/managment/clients/[id]/files/page.tsx
@@ -0,0 +1,7 @@
+export default function ClientFilesPage() {
+  return (
+    <div>
+      <h1>ClientFilesPage</h1>
+    </div>
+  );
+}
--- src/app/managment/clients/page.tsx
+++ src/app/managment/clients/page.tsx
@@ -13,6 +13,7 @@ import {
 } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { CirclePlus, Trash } from 'lucide-react';
+import Link from 'next/link';
 
 export default function ClientsPage() {
   return (
@@ -25,15 +26,17 @@ export default function ClientsPage() {
       <Separator className="mt-4" />
       <div className="grid grid-cols-3 gap-2 mt-4">
         <Card>
-          <CardHeader>
-            <CardTitle>Client 1</CardTitle>
-            <CardDescription>164 Files uploaded</CardDescription>
-          </CardHeader>
-          <CardFooter className="flex justify-end">
-            <Button variant="outline" size="icon">
-              <Trash />
-            </Button>
-          </CardFooter>
+          <Link href="/clients/files">
+            <CardHeader>
+              <CardTitle>Client 1</CardTitle>
+              <CardDescription>164 Files uploaded</CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-end">
+              <Button variant="outline" size="icon">
+                <Trash />
+              </Button>
+            </CardFooter>
+          </Link>
         </Card>
         <button className="border border-dashed border-border flex flex-col justify-center items-center gap-1 bg-background hover:brightness-75">
           <CirclePlus size={32} />
